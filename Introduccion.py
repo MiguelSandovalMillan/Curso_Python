82d7930fa207e07af317a2d387ba45e2b6b2215b
MiguelSandovalMillan <2408819j@umich.mx>
2026-01-09 15:11:05 -0600

    assasas

---
+++ Introduccion.py
@@ -0,0 +1,15 @@
+
+print("Hola juan16ito")
+print('Hola juan16ito')
+
+# Primer tipo de comentario
+
+"""
+Otra forma de poner
+comentario
+"""
+
+'''
+Tambien es otro comentario
+en varias comillas
+'''
