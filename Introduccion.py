52080b5065167e556aa06cfd271908ca6ebef956
MiguelSandovalMillan <2408819j@umich.mx>
2026-01-09 15:11:15 -0600

    asas

--- Introduccion.py
+++ Introduccion.py
@@ -13,3 +13,10 @@ comentario
 Tambien es otro comentario
 en varias comillas
 '''
+
+
+print(type("Hola juan"))
+print(type(1))
+print(type(2.5))
+print(type(1 + 3j))
+print(type(True))
---
+++ Variables.py
@@ -0,0 +1,17 @@
+# Variables
+
+Usuario = "Juan16ito"
+Edad = 20
+Altura = 1.73
+
+
+print(Usuario)
+print(Edad)
+print(Altura)
+
+
+print(Usuario,Edad,Altura) 
+
+Nueva = str(Edad)
+print(Nueva)
+print(type(Nueva)) 
